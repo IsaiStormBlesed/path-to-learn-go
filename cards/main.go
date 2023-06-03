dbe6b388779103e8fae4d383524c37400e311332
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2023-06-03 23:44:10 +0000

    slices & range

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+    cards := []string{"A", "B", "C"}
+    cards = append(cards, "D")
+
+    for _, card := range cards {
+        fmt.Println(card)
+    }
+}
+
+func newCards() string {
+    return "Ace of Spades"
 }
