15df9f66659a3eca020f31d99e987594fc77686d
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2023-06-05 17:01:34 +0000

    print & newDeck func

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-    cards := []string{"A", "B", "C"}
-    cards = append(cards, "D")
-
-    for _, card := range cards {
-        fmt.Println(card)
-    }
-}
-
-func newCards() string {
-    return "Ace of Spades"
+	cards := newDeck()
+	cards.print()
 }
